9dc74fbd2d1b8564cf6fe5bd9b42f9b957e09d18
Jack Spencer Walton <132408856+jack-walton@users.noreply.github.com>
2025-09-09 23:41:47 -0500

    Refactor code
    I updated the indentation for clarity

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -23,12 +23,12 @@ useSeoMeta(page.value?.seo || {})
 
 <template>
   <article>
-  <NuxtLayout :name="page?.layout as LayoutKey || 'default'" class="bg-white dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700">
-    <ContentRenderer
-      v-if="page"
-      :value="page"
-      class="prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-900"
-    />
-  </NuxtLayout>
-</article>
+    <NuxtLayout :name="page?.layout as LayoutKey || 'default'" class="bg-white dark:bg-gray-800 ring-1 ring-gray-200 dark:ring-gray-700">
+      <ContentRenderer
+        v-if="page"
+        :value="page"
+        class="prose dark:prose-invert prose-pre:bg-gray-100 dark:prose-pre:bg-gray-900"
+      />
+    </NuxtLayout>
+  </article>
 </template>
